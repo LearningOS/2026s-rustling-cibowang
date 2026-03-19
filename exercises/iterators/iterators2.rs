35b4cadf7fabbe2173667ca537e634cdf22d307a
cibowang <980368160@qq.com>
2026-03-19 16:20:05 +0800

    update

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::ops::Deref;
 
 // Step 1.
@@ -17,7 +15,8 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.to_uppercase().collect(),
+        // still need to convert char into String
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
@@ -26,9 +25,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    let mut v = vec![];
-    v.push(capitalize_first(words));
-    v
+    words.to_owned().into_iter().map(capitalize_first).collect()
 }
 
 // Step 3.
@@ -36,8 +33,7 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    let mut s = String::new();
-    s
+    words.to_owned().into_iter().map(capitalize_first).collect()
 }
 
 #[cfg(test)]
--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+use std::{num, thread::AccessError};
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +15,9 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    // let factorial_num = num * (num - 1) * (num - 2).. * (num - n)
+    // fold is =+*
+    (1..num + 1).fold(1, |acc: u64, x: u64| acc * x)
 }
 
 #[cfg(test)]
